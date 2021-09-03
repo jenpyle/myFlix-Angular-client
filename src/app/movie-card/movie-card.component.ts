94defb1a52060de0af1fb2d8023e65dce63ac9fe
jenpyle <jennypyle440@gmail.com>
2021-09-03 17:11:09 -0600

    fill in basics on user profile component

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -2,6 +2,8 @@
 // has a "template." The template (i.e., the .component.html file) is rendered by the component
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
+import { UserProfileComponent } from '../user-profile/user-profile.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-card',
@@ -9,9 +11,15 @@ import { FetchApiDataService } from '../fetch-api-data.service';
   styleUrls: ['./movie-card.component.scss'],
 })
 export class MovieCardComponent {
+  isLoading = false;
+  //This is where the user info returned from the API call will be kept.
+  userInfo: any = {};
   //This is where the movies returned from the API call will be kept
   movies: any[] = [];
-  constructor(public fetchApiData: FetchApiDataService) {}
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public router: Router
+  ) {}
 
   //After implementing the function getMovies(), it's then called in the ngOnInit() lifecycle hook
   //ngOnInit() is called when Angular is done creating the component
@@ -27,5 +35,17 @@ export class MovieCardComponent {
       console.log(this.movies);
       return this.movies;
     });
+  }
+
+  routeToProfile(): void {
+    this.isLoading = true;
+    // const user = localStorage.getItem('user');
+    // this.fetchApiData.getUserData(user).subscribe((resp: any) => {
+    //   this.isLoading = true;
+    //   this.userInfo = resp;
+    //   console.log(this.userInfo);
+    this.router.navigate(['profile']);
+    // return this.userInfo;
+    // });
   }
 }
